534b24fd232226fbffcf91823b40947a07233f44
Brenda <Brenda@DESKTOP-2P3L4JQ>
2024-11-21 22:55:01 -0600

    ui changes

--- AirportManagementSoftware/Login.h
+++ AirportManagementSoftware/Login.h
@@ -1,1 +1,37 @@
-#pragma once
+//  << control class >>
+//     Log - In
+
+/*
+- username : string
+- password : string
++ createAccount(username : String; password: String) : boolean
++ login(username : String; password: String) : boolean
++ clockIn(username : String; password: String) : boolean
++ deleteAccount(username : String) : boolean
++ logout() : void
++ registerNewUser(userData : string) : boolean
++ obtainInput() : void
+
+*/
+
+
+#ifndef LOGIN_H
+#define LOGIN_H
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+class LOGIN {
+
+private:
+	string username;
+	string password;
+
+public:
+
+
+
+};
+
+#endif 
--- AirportManagementSoftware/UserInterface.cpp
+++ AirportManagementSoftware/UserInterface.cpp
@@ -5,6 +5,12 @@ using namespace std;
 
 // FUNCTION TO DISPLAY THE LOGIN PROMPT
 void UserInterface::displayLoginScreen() {
+    cout << "=================" << endl;
+    cout << "  W|E|L|C|O|M|E" << endl;
+    cout << "=================" << endl;
+    cout << "Please enter your username and password" << endl;
+
+    //Username and passwords objects
     string username, password;
 
     cout << "Enter username: ";
@@ -12,7 +18,13 @@ void UserInterface::displayLoginScreen() {
 
     cout << "Enter password: ";
     cin >> password;
-
+   
+    if (validateLogin(username, password)) {
+        cout << "Login was successful" << endl;
+    } else {
+        cout << "Incorrect Login" << endl;
+        
+    }
     // validate username and password here
 }
 
