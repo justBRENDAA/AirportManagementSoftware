e8f9f1a7903033337b1ed07e02387694856a0263
1chasey <chase42400@icloud.com>
2024-11-23 10:03:49 -0600

    luggage commit

---
+++ AirportManagementSoftware/Luggage.cpp
@@ -0,0 +1,12 @@
+#include "UserInterface.h"
+#include <iostream>
+
+using namespace std;  
+
+// FUNCTION TO DISPLAY THE LOGIN PROMPT
+void UserInterface::displayLoginScreen() {
+    cout << "=================" << endl;
+    cout << "  W|E|L|C|O|M|E" << endl;
+    cout << "=================" << endl;
+    cout << "Please enter your username and password" << endl;
+}
