073d0e791e3636196c4f50adea069fa433f3110a
Brenda <Brenda@DESKTOP-2P3L4JQ>
2024-11-28 23:16:00 -0600

    modified luggage class added private variables and modified displayInfo method

--- AirportManagementSoftware/Luggage.cpp
+++ AirportManagementSoftware/Luggage.cpp
@@ -3,7 +3,33 @@
 #include <cppconn/prepared_statement.h>
 #include <cppconn/resultset.h>
 
-Luggage::Luggage(sql::Connection* existingCon, std::string username)
+Luggage::Luggage(sql::Connection* existingCon, std::string user)
 {
+	con = existingCon;
+	username = user;
+}
 
+void Luggage::displayInfo()
+{
+    sql::PreparedStatement* pstmt = con->prepareStatement(
+        "SELECT luggage_id, passenger_id, flight_id, location FROM Luggage WHERE username = ?"
+    );
+    pstmt->setString(1, username);
+
+    sql::ResultSet* resLuggage = pstmt->executeQuery();
+    if (resLuggage->next()) {
+        luggage_id = resLuggage->getString("luggage_id");
+        passenger_id = resLuggage->getString("passenger_id");
+        flight_id = resLuggage->getString("flight_id");
+        location = resLuggage->getString("location");
+    }
+    delete resLuggage;
+    delete pstmt;
+
+    std::cout << "Luggage Information\n";
+    std::cout << "=====================\n";
+    std::cout << "Luggage ID: " << luggage_id << std::endl;
+    std::cout << "Passenger ID: " << passenger_id << std::endl;
+    std::cout << "Flight ID: " << flight_id << std::endl;
+    std::cout << "Location: " << location << std::endl;
 }
--- AirportManagementSoftware/Luggage.h
+++ AirportManagementSoftware/Luggage.h
@@ -2,20 +2,23 @@
 #define LUGGAGE_H
 
 #include <string>
-#include <cppconn/prepared_statement.h>
-#include <cppconn/resultset.h>
+#include <mysql_driver.h>
+#include <mysql_connection.h>
 
 
 class Luggage {
 private:
+    std::string username;
     std::string luggage_id;
     std::string passenger_id;
     std::string flight_id;
     std::string location;
+    
+    sql::Connection* con;
 
 public:
     Luggage(sql::Connection* existingCon, std::string username);
-
+    void displayInfo();
 };
 
 #endif
